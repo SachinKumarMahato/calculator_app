b2fb3ea05d75df61f62ad924c26936de4f5a3cf8
Sachin <sachin333777@outlook.com>
2024-01-22 07:31:56 +0530

    add IIFE

--- src/js/main.js
+++ src/js/main.js
@@ -1,106 +1,108 @@
-const buttons = document.querySelectorAll('.btn');
-const input = document.querySelector('input');
-const operators = document.querySelectorAll('.operator');
-const reset = document.querySelector('.reset');
-const equal = document.querySelector('.equal');
-const del = document.querySelector('.del');
-const dot = document.querySelector('.dot');
+(function Calculator() {
+  const buttons = document.querySelectorAll('.btn');
+  const input = document.querySelector('input');
+  const operators = document.querySelectorAll('.operator');
+  const reset = document.querySelector('.reset');
+  const equal = document.querySelector('.equal');
+  const del = document.querySelector('.del');
+  const dot = document.querySelector('.dot');
 
-let currentValue = '';
-let operator = null;
-let currSum = 0;
-let prevOperator = null;
+  let currentValue = '';
+  let operator = null;
+  let currSum = 0;
+  let prevOperator = null;
 
-function handleButtonClick(button) {
-  const value = (button.innerText);
-  currentValue += value;
-  input.value += value;
-}
-
-function add(currVal, prevVal, op) {
-  const currNum = parseFloat(currVal);
-  const prevNum = parseFloat(prevVal);
-
-  switch (op) {
-    case '+':
-      return ((currNum + prevNum)).toString();
-
-    case '-':
-      return ((currNum - prevNum)).toString();
-
-    case 'x':
-      return ((currNum * prevNum)).toString();
-
-    case '/':
-      return ((currNum / prevNum)).toString();
-
-    default:
-      throw Error('something wrong');
+  function handleButtonClick(button) {
+    const value = (button.innerText);
+    currentValue += value;
+    input.value += value;
   }
-}
-function handleOperationClick(op) {
-  try {
-    const operatorVal = op.innerText;
-    operator = operatorVal;
-    if (currentValue !== '') {
-      if (currSum === 0) {
-        currSum = currentValue;
-      } else {
-        currSum = add(currSum, currentValue, prevOperator);
+
+  function Calculate(currVal, prevVal, op) {
+    const currNum = parseFloat(currVal);
+    const prevNum = parseFloat(prevVal);
+
+    switch (op) {
+      case '+':
+        return ((currNum + prevNum)).toString();
+
+      case '-':
+        return ((currNum - prevNum)).toString();
+
+      case 'x':
+        return ((currNum * prevNum)).toString();
+
+      case '/':
+        return ((currNum / prevNum)).toString();
+
+      default:
+        throw Error('something wrong');
+    }
+  }
+  function handleOperationClick(op) {
+    try {
+      const operatorVal = op.innerText;
+      operator = operatorVal;
+      if (currentValue !== '') {
+        if (currSum === 0) {
+          currSum = currentValue;
+        } else {
+          currSum = Calculate(currSum, currentValue, prevOperator);
+        }
       }
-    }
-    currentValue = '';
-    input.value += operatorVal;
-    prevOperator = operator;
-  } catch (err) {
-    throw new Error(err);
-  }
-}
-function handleResetClick() {
-  input.value = '';
-  currentValue = '';
-  currSum = 0;
-  operator = null;
-}
-function handleEqualClick() {
-  try {
-    if (equal.innerText === '=') {
-      currSum = add(currSum, currentValue, operator);
       currentValue = '';
-      operator = null;
-      input.value = currSum;
+      input.value += operatorVal;
+      prevOperator = operator;
+    } catch (err) {
+      throw new Error(err);
     }
-  } catch (error) {
-    throw new Error(error);
   }
-}
+  function handleResetClick() {
+    input.value = '';
+    currentValue = '';
+    currSum = 0;
+    operator = null;
+  }
+  function handleEqualClick() {
+    try {
+      if (equal.innerText === '=') {
+        currSum = Calculate(currSum, currentValue, operator);
+        currentValue = '';
+        operator = null;
+        input.value = currSum;
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    handleButtonClick(button);
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      handleButtonClick(button);
+    });
   });
-});
 
-operators.forEach((op) => {
-  op.addEventListener('click', () => {
-    handleOperationClick(op);
+  operators.forEach((op) => {
+    op.addEventListener('click', () => {
+      handleOperationClick(op);
+    });
   });
-});
 
-reset.addEventListener('click', handleResetClick);
-del.addEventListener('click', () => {
-  if (input.value !== '') {
-    input.value = input.value.slice(0, -1);
-  }
-});
-dot.addEventListener('click', () => {
-  if (!dot.disabled && !input.value.includes('.')) {
-    dot.disabled = true;
-  }
-});
+  reset.addEventListener('click', handleResetClick);
+  del.addEventListener('click', () => {
+    if (input.value !== '') {
+      input.value = input.value.slice(0, -1);
+    }
+  });
+  dot.addEventListener('click', () => {
+    if (!dot.disabled && !input.value.includes('.')) {
+      dot.disabled = true;
+    }
+  });
 
-equal.addEventListener('click', handleEqualClick);
+  equal.addEventListener('click', handleEqualClick);
 
-document.addEventListener('keydown', (event) => {
-  event.preventDefault();
-});
+  document.addEventListener('keydown', (event) => {
+    event.preventDefault();
+  });
+}());
